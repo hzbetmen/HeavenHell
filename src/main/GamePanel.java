e9954df58e1de1949f23c0bf8c154b228da26282
Bach <dangvanbach5522@gmail.com>
2022-12-07 16:23:31 +0100

    Bach update

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -6,8 +6,6 @@ import Inputs.MouseInputs;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,6 +19,9 @@ public class GamePanel extends JPanel {
 
     private BufferedImage img, idleAni[];
     private int aniTick, aniIndex, aniSpeed = 30;
+    private final int imgTile = 16;
+    private int blockScale = 4;
+    private int imgHeight = imgTile * blockScale, imgWidth = imgTile * blockScale;
 
 
     public GamePanel() {
@@ -38,7 +39,7 @@ public class GamePanel extends JPanel {
     }
 
     private void importImg() {
-        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+        InputStream is = getClass().getResourceAsStream("/FLOOR_HELL.png");
 
         try {
             img = ImageIO.read(is);
@@ -55,10 +56,10 @@ public class GamePanel extends JPanel {
 
     //private void ani
     private void loadAnimations() {
-        idleAni = new BufferedImage[5];
-        for (int i = 1; i < idleAni.length; i++) {
-            idleAni[i] = img.getSubimage(i*64, 0, 64, 40);
-        }
+//        idleAni = new BufferedImage[2];
+//        for (int i = 0; i < idleAni.length; i++) {
+//            idleAni[i] = img.getSubimage(i*8, 0, 8, 16);
+//        }
     }
 
     private void setPanelSize() {
@@ -71,7 +72,7 @@ public class GamePanel extends JPanel {
         super.paintComponent(g);
 
 
-        g.drawImage(idleAni[2], (int) xDelta, (int) yDelta, 128, 80, null);
+        g.drawImage(img, (int) xDelta, (int) yDelta, imgWidth, imgHeight, null);
 
 
     }
